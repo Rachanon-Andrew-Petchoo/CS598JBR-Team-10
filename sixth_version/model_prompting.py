b6e5264da8fadd7041fad4a0f8891bd78c426d1b
Rachanon Petchoo <petchoo2@illinois.edu>
2025-09-23 16:36:52 -0500

    "Fixed"

--- sixth_version/model_prompting.py
+++ sixth_version/model_prompting.py
@@ -82,7 +82,8 @@ def prompt_model(dataset, model_name = "deepseek-ai/deepseek-coder-6.7b-base", q
         with torch.no_grad():
             output_ids = model.generate(
                 **inputs,
-                max_new_tokens=500,      # temperature = 0
+                max_new_tokens=500,
+                do_sample=True,
                 temperature=0.0,
                 eos_token_id=tokenizer.eos_token_id,
                 pad_token_id=tokenizer.pad_token_id,
